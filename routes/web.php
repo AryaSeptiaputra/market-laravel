1980480bba747f9ab74cbbccca00f490db9ef42c
AryaSeptiaputra <aryaekaseptiaputra016@gmail.com>
2023-12-31 11:46:52 +0700

    Halaman login dan registrasi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +12,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Sistem Login
+Route::prefix('auth')->group(function() {
+    Route::get('login',[AuthenticationController::class, 'showLoginForm'])->middleware('guest');
+    Route::get('register',[AuthenticationController::class, 'showRegistrationForm'])->middleware('guest');
+    Route::post('register',[AuthenticationController::class, 'register'])->middleware('guest');
+    Route::post('login',[AuthenticationController::class, 'login'])->middleware('guest')->name('login');
+    Route::post('logout',[AuthenticationController::class, 'logout'])->middleware('auth');
+});
 
 Route::get('/', function () {
     return view('welcome');
